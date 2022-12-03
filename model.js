0eeec08115ad829356f3e1a642583e5cf4f62ac5
Reiter18 <118561820+Reiter18@users.noreply.github.com>
2022-12-03 20:12:29 +0500

    Add files via upload

---
+++ model.js
@@ -0,0 +1,10 @@
+export const model = [
+    {type: 'title', value: 'Hello World from JS!!!!'},
+    {type: 'text', value: 'here we go with some text'},
+    {type: 'colomns', value: [
+        '111111111',
+        '222222222',
+        '333333333'
+    ]},
+    {type: 'image', value: './src/image.jpg'}
+    ]
